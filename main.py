763b25c5cef73a25608a5c0e6531b209530e4195
0aqz0 <18026332256@163.com>
2020-01-11 21:33:33 +0800

    3d cnn converges

--- main.py
+++ main.py
@@ -1,72 +1,91 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import torchvision.transforms as transforms
+from sklearn.metrics import accuracy_score
 from models import CNN3D
 from dataset import CSL_Dataset
 
-PATH = './slr_cnn3d.pth'
+# Path setting
+data_path = "/home/ddq/Data/CSL_Dataset/S500_color_video"
+label_path = "/home/ddq/Data/CSL_Dataset/dictionary.txt"
+model_path = "./slr_cnn3d.pth"
+log_path = "./log.txt"
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Device setting
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparams
-batch_size = 16
-learning_rate = 1e-3
-max_epochs = 16
+num_classes = 100
+epochs = 15
+batch_size = 30
+learning_rate = 1e-4
+log_interval = 10
+img_d, img_h, img_w = 25, 128, 96
+drop_p = 0.0
+hidden1, hidden2 = 256, 256
+
 
 if __name__ == '__main__':
     # Train with 3DCNN
-    transform = transforms.Compose([transforms.Resize([64, 48]), transforms.ToTensor()])
-    dataset = CSL_Dataset(data_path="/home/ddq/Data/origin/S500_color_video",
-        label_path='/home/ddq/Data/origin/dictionary.txt', transform=transform)
+    # Load data
+    transform = transforms.Compose([transforms.Resize([img_h, img_w]),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=[0.5], std=[0.5])])
+    dataset = CSL_Dataset(data_path=data_path, label_path=label_path, frames=img_d, transform=transform)
     print("Dataset samples: {}".format(len(dataset)))
-    trainloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-    cnn3d = CNN3D()
-    cnn3d.to(device)
+    trainloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    # Create model
+    cnn3d = CNN3D(img_depth=img_d, img_height=img_h, img_width=img_w, drop_p=drop_p,
+                hidden1=hidden1, hidden2=hidden2, num_classes=num_classes).to(device)
+    # Run the model parallelly
+    if torch.cuda.device_count() > 1:
+        print("Using {} GPUs".format(torch.cuda.device_count()))
+        cnn3d = nn.DataParallel(cnn3d)
+    # Create loss criterion & optimizer & log writer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(cnn3d.parameters(), lr=learning_rate)
-    writer = SummaryWriter('runs/slr')
-    print("Training Started".center(40, '#'))
-    for epoch in range(max_epochs):
+    # writer = SummaryWriter('runs/slr')
+    # log = open(log_path, "w")
+
+    # Start training
+    print("Training Started".center(80, '#'))
+    for epoch in range(epochs):
+        # Set trainning mode
+        cnn3d.train()
+
         running_loss = 0.0
-        running_acc = 0.0
-        iteration_count = 0
 
         for i, data in enumerate(trainloader):
             # get the inputs and labels
             inputs, labels = data['images'].to(device), data['label'].to(device)
 
-            # zero the gradient
             optimizer.zero_grad()
-
             # forward & backward & optimize
             outputs = cnn3d(inputs)
-            loss = criterion(outputs, labels.squeeze())
+            loss = F.cross_entropy(outputs, labels.squeeze())
+
+            # compute the loss
+            running_loss += loss.item()
+
+            # compute the accuracy
+            prediction = torch.max(outputs, 1)[1]
+            score = accuracy_score(labels.squeeze().cpu().data.squeeze().numpy(), prediction.cpu().data.squeeze().numpy())
+
             loss.backward()
             optimizer.step()
 
-            # calculate the loss
-            running_loss += loss.item()
-
-            # calculate the acc
-            acc = torch.sum((torch.argmax(outputs, 1) == labels)) / batch_size
-            # print(outputs, labels)
-            # print((torch.argmax(outputs, 1)))
-            # count iteration
-            # iteration_count += 1
-
-            if i % 4 == 3:
-                print("epoch {} | iteration {:5d} | Loss {} | Acc {}".format(epoch+1, i+1, running_loss/4, acc))
-                writer.add_scalar('training loss', running_loss/4, i+1)
+            if (i + 1) % log_interval == 0:
+                print("epoch {:3d} | iteration {:5d} | Loss {:.6f} | Acc {:.2f}%".format(epoch+1, i+1, loss.item(), score*100))
+                # writer.add_scalar('training loss', running_loss/4, i+1)
                 running_loss = 0.0
+                acc = 0.0
 
-        # print log file
-        # print("Epoch {} | Loss {} | Acc {}".format(epoch, running_loss / iteration, running_acc / iteration))
-
-    print("Training Finished".center(40, '#'))
+    print("Training Finished".center(80, '#'))
 
     # Save model
-    torch.save(cnn3d.state_dict(), PATH)
-    print("Model Saved".center(40, '#'))
+    torch.save(cnn3d.state_dict(), model_path)
+    print("Model Saved".center(80, '#'))
