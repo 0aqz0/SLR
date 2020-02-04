6adb961571ef4bc7f335e8fd2cf83f612a8f1065
0aqz0 <18026332256@163.com>
2020-02-04 15:44:02 +0800

    use 125000 as CSL_Isolated

--- CSL_Isolated_Conv3D.py
+++ CSL_Isolated_Conv3D.py
@@ -31,12 +31,12 @@ logger.info('Logging to file...')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparams
-num_classes = 500
+num_classes = 100
 epochs = 60
-batch_size = 64
+batch_size = 32
 learning_rate = 1e-4
-log_interval = 10
-img_d, img_h, img_w = 16, 128, 96
+log_interval = 20
+img_d, img_h, img_w = 16, 128, 128
 drop_p = 0.0
 hidden1, hidden2 = 512, 256
 
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     transform = transforms.Compose([transforms.Resize([img_h, img_w]),
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.5], std=[0.5])])
-    dataset = CSL_Isolated(data_path=data_path, label_path=label_path, frames=img_d, transform=transform)
+    dataset = CSL_Isolated(data_path=data_path, label_path=label_path, frames=img_d, num_classes=num_classes, transform=transform)
     trainset, testset = random_split(dataset, [int(0.8*len(dataset)), int(0.2*len(dataset))])
     logger.info("Dataset samples: {}".format(len(dataset)))
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
--- dataset.py
+++ dataset.py
@@ -5,11 +5,79 @@ from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
 """
-Implementation of Chinese Sign Language Dataset
+Implementation of Chinese Sign Language Dataset(50 signers with 5 times)
 """
 class CSL_Isolated(Dataset):
-    def __init__(self, data_path, label_path, frames=16, videos_per_folder=250, transform=None):
+    def __init__(self, data_path, label_path, frames=16, num_classes=500, transform=None):
         super(CSL_Isolated, self).__init__()
+        self.data_path = data_path
+        self.label_path = label_path
+        self.transform = transform
+        self.frames = frames
+        self.num_classes = num_classes
+        self.signers = 50
+        self.repetition = 5
+        self.videos_per_folder = self.signers * self.repetition
+        self.data_folder = []
+        try:
+            obs_path = [os.path.join(self.data_path, item) for item in os.listdir(self.data_path)]
+            self.data_folder = sorted([item for item in obs_path if os.path.isdir(item)])
+        except Exception as e:
+            print("Something wrong with your data path!!!")
+            raise
+        self.labels = {}
+        try:
+            label_file = open(self.label_path, 'r')
+            for line in label_file.readlines():
+                line = line.strip()
+                line = line.split('\t')
+                self.labels[line[0]] = line[1]
+        except Exception as e:
+            raise
+
+    def read_images(self, folder_path):
+        assert len(os.listdir(folder_path)) >= self.frames, "Too few images in your data folder: " + str(folder_path)
+        images = []
+        start = 1
+        step = int(len(os.listdir(folder_path))/self.frames)
+        for i in range(self.frames):
+            image = Image.open(os.path.join(folder_path, '{:06d}.jpg').format(start+i*step)).convert('L')
+            if self.transform is not None:
+                image = self.transform(image)
+            images.append(image)
+
+        images = torch.stack(images, dim=0)
+        # switch dimension for 3d cnn
+        images = images.permute(1, 0, 2, 3)
+        # print(images.shape)
+        return images
+
+    def __len__(self):
+        return self.num_classes * self.videos_per_folder
+
+    def __getitem__(self, idx):
+        top_folder = self.data_folder[int(idx/self.videos_per_folder)]
+        selected_folders = [os.path.join(top_folder, item) for item in os.listdir(top_folder)]
+        selected_folders = sorted([item for item in selected_folders if os.path.isdir(item)])
+        selected_folder = selected_folders[idx%self.videos_per_folder]
+        images = self.read_images(selected_folder)
+        # print(selected_folder, int(idx/self.videos_per_folder))
+        # print(self.labels['{:06d}'.format(int(idx/self.videos_per_folder))])
+        # label = self.labels['{:06d}'.format(int(idx/self.videos_per_folder))]
+        label = torch.LongTensor([int(idx/self.videos_per_folder)])
+
+        return {'images': images, 'label': label}
+
+    def label_to_word(self, label):
+        return self.labels['{:06d}'.format(label.item())]
+
+
+"""
+Implementation of Chinese Sign Language Dataset(50 signers with 1 time)
+"""
+class CSL_Isolated_25000(Dataset):
+    def __init__(self, data_path, label_path, frames=16, videos_per_folder=250, transform=None):
+        super(CSL_Isolated_25000, self).__init__()
         self.data_path = data_path
         self.label_path = label_path
         self.transform = transform
@@ -96,10 +164,10 @@ class CSL_Skeleton(Dataset):
 
 # Test
 if __name__ == '__main__':
-    transform = transforms.Compose([transforms.Resize([128, 96]), transforms.ToTensor()])
+    transform = transforms.Compose([transforms.Resize([128, 128]), transforms.ToTensor()])
     dataset = CSL_Isolated(data_path="/home/aistudio/data/data20273/CSL_Isolated_125000",
         label_path='/home/aistudio/data/data20273/CSL_Isolated_125000/dictionary.txt', transform=transform)
-    print(len(dataset))
-    print(dataset[1000]['images'].shape)
+    # print(len(dataset))
+    # print(dataset[1000]['images'].shape)
     label = dataset[1000]['label']
     print(dataset.label_to_word(label))
