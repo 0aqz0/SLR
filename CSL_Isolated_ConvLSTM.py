01cc0a3676978e31e1a2b497475bdc528a16c071
0aqz0 <18026332256@163.com>
2020-02-05 10:59:27 +0800

    cnn+lstm achieves acc 94.88% on 100 classes at epoch 59

--- CSL_Isolated_ConvLSTM.py
+++ CSL_Isolated_ConvLSTM.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from datetime import datetime
+import logging
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,27 +9,26 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 import torchvision.transforms as transforms
-from sklearn.metrics import accuracy_score
-from models import CNN3D, CRNN
+from models.ConvLSTM import CRNN
 from dataset import CSL_Isolated
+from train import train
+from test import test
 
 # Path setting
-data_path = "/home/aistudio/data/data20273/CSL_Isolated_125000"
-label_path = "/home/aistudio/data/data20273/CSL_Isolated_125000/dictionary.txt"
+data_path = "/home/haodong/Data/CSL_Isolated/color_video_125000"
+label_path = "/home/haodong/Data/CSL_Isolated/dictionary.txt"
 model_path = "."
-log_path = "./log{:_%Y-%m-%d_%H-%M-%S}.txt".format(datetime.now())
-sum_path = "runs/slr{:_%Y-%m-%d_%H-%M-%S}".format(datetime.now())
+log_path = "log/{:%Y-%m-%d_%H-%M-%S}.log".format(datetime.now())
+sum_path = "runs/slr_{:%Y-%m-%d_%H-%M-%S}".format(datetime.now())
 
 # Log to file & tensorboard writer
-log_to_file = False
-if log_to_file:
-    log = open(log_path, "w")
-    sys.stdout = log
-    print("Logging to file...")
+logging.basicConfig(level=logging.INFO, format='%(message)s', handlers=[logging.FileHandler(log_path), logging.StreamHandler()])
+logger = logging.getLogger('SLR')
+logger.info('Logging to file...')
 writer = SummaryWriter(sum_path)
 
 # Use specific gpus
-os.environ["CUDA_VISIBLE_DEVICES"]="1, 2"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 # Device setting
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -37,108 +37,43 @@ num_classes = 100
 epochs = 60
 batch_size = 32
 learning_rate = 1e-4
-log_interval = 10
-img_d, img_h, img_w = 25, 128, 96
+log_interval = 20
+img_d, img_h, img_w = 16, 128, 128
+drop_p = 0.0
+hidden1, hidden2 = 512, 256
 
-
+# Train with Conv+LSTM
 if __name__ == '__main__':
-    # Train
     # Load data
     transform = transforms.Compose([transforms.Resize([img_h, img_w]),
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.5], std=[0.5])])
-    dataset = CSL_Isolated(data_path=data_path, label_path=label_path, frames=img_d, transform=transform)
+    dataset = CSL_Isolated(data_path=data_path, label_path=label_path, frames=img_d, num_classes=num_classes, transform=transform)
     trainset, testset = random_split(dataset, [int(0.8*len(dataset)), int(0.2*len(dataset))])
-    print("Dataset samples: {}".format(len(dataset)))
-    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-    testloader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    logger.info("Dataset samples: {}".format(len(dataset)))
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
     # Create model
-    # model = CNN3D(img_depth=img_d, img_height=img_h, img_width=img_w, num_classes=num_classes).to(device)
     model = CRNN(img_depth=img_d, img_height=img_h, img_width=img_w, num_classes=num_classes).to(device)
     # Run the model parallelly
     if torch.cuda.device_count() > 1:
-        print("Using {} GPUs".format(torch.cuda.device_count()))
+        logger.info("Using {} GPUs".format(torch.cuda.device_count()))
         model = nn.DataParallel(model)
     # Create loss criterion & optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     # Start training
-    print("Training Started".center(60, '#'))
+    logger.info("Training Started".center(60, '#'))
     for epoch in range(epochs):
-        # Set trainning mode
-        model.train()
-        losses = []
-        all_label = []
-        all_pred = []
-
-        for batch_idx, data in enumerate(trainloader):
-            # get the inputs and labels
-            inputs, labels = data['images'].to(device), data['label'].to(device)
-
-            optimizer.zero_grad()
-            # forward
-            outputs = model(inputs)
-
-            # compute the loss
-            loss = criterion(outputs, labels.squeeze())
-            losses.append(loss.item())
-
-            # compute the accuracy
-            prediction = torch.max(outputs, 1)[1]
-            all_label.extend(labels.squeeze())
-            all_pred.extend(prediction)
-            score = accuracy_score(labels.squeeze().cpu().data.squeeze().numpy(), prediction.cpu().data.squeeze().numpy())
-
-            # backward & optimize
-            loss.backward()
-            optimizer.step()
-
-            if (batch_idx + 1) % log_interval == 0:
-                print("epoch {:3d} | iteration {:5d} | Loss {:.6f} | Acc {:.2f}%".format(epoch+1, batch_idx+1, loss.item(), score*100))
-
-        # Compute the average loss & accuracy
-        training_loss = sum(losses)/len(losses)
-        all_label = torch.stack(all_label, dim=0)
-        all_pred = torch.stack(all_pred, dim=0)
-        training_acc = accuracy_score(all_label.squeeze().cpu().data.squeeze().numpy(), all_pred.cpu().data.squeeze().numpy())
-        # Log
-        writer.add_scalar('Loss/train', training_loss, epoch+1)
-        writer.add_scalar('Accuracy/train', training_acc, epoch+1)
-        print("Average Training Loss of Epoch {}: {:.6f} | Acc: {:.2f}%".format(epoch+1, training_loss, training_acc*100))
+        # Train the model
+        train(model, criterion, optimizer, trainloader, device, epoch, logger, log_interval, writer)
 
         # Test the model
-        # Set testing mode
-        model.eval()
-        losses = []
-        all_label = []
-        all_pred = []
-
-        with torch.no_grad():
-            for batch_idx, data in enumerate(testloader):
-                # get the inputs and labels
-                inputs, labels = data['images'].to(device), data['label'].to(device)
-                # forward
-                outputs = model(inputs)
-                # compute the loss
-                loss = criterion(outputs, labels.squeeze())
-                losses.append(loss.item())
-                # collect labels & prediction
-                prediction = torch.max(outputs, 1)[1]
-                all_label.extend(labels.squeeze())
-                all_pred.extend(prediction)
-        # Compute the average loss & accuracy
-        testing_loss = sum(losses)/len(losses)
-        all_label = torch.stack(all_label, dim=0)
-        all_pred = torch.stack(all_pred, dim=0)
-        testing_acc = accuracy_score(all_label.squeeze().cpu().data.squeeze().numpy(), all_pred.cpu().data.squeeze().numpy())
-        # Log
-        writer.add_scalar('Loss/test', testing_loss, epoch+1)
-        writer.add_scalar('Accuracy/test', testing_acc, epoch+1)
-        print("Average Testing Loss of Epoch {}: {:.6f} | Acc: {:.2f}%".format(epoch+1, testing_loss, testing_acc*100))
+        test(model, criterion, testloader, device, epoch, logger, writer)
 
         # Save model
-        torch.save(model.state_dict(), os.path.join(model_path, "slr_lstm_epoch{}.pth".format(epoch+1)))
-        print("Epoch {} Model Saved".format(epoch+1).center(60, '#'))
+        torch.save(model.state_dict(), os.path.join(model_path, "slr_convlstm_epoch{}.pth".format(epoch+1)))
+        logger.info("Epoch {} Model Saved".format(epoch+1).center(60, '#'))
 
-    print("Training Finished".center(60, '#'))
+    logger.info("Training Finished".center(60, '#'))
