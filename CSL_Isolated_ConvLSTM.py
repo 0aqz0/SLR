267ba16a1ff2297bbb8091772651902de3c92c90
0aqz0 <18026332256@163.com>
2020-02-06 19:12:39 +0800

    cnn+lstm achieves acc 91.09% on 500 classes at epoch 91

--- CSL_Isolated_ConvLSTM.py
+++ CSL_Isolated_ConvLSTM.py
@@ -17,7 +17,7 @@ from test import test
 # Path setting
 data_path = "/home/haodong/Data/CSL_Isolated/color_video_125000"
 label_path = "/home/haodong/Data/CSL_Isolated/dictionary.txt"
-model_path = "."
+model_path = "/home/haodong/Data/saved_models"
 log_path = "log/{:%Y-%m-%d_%H-%M-%S}.log".format(datetime.now())
 sum_path = "runs/slr_{:%Y-%m-%d_%H-%M-%S}".format(datetime.now())
 
@@ -28,19 +28,17 @@ logger.info('Logging to file...')
 writer = SummaryWriter(sum_path)
 
 # Use specific gpus
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="1,3"
 # Device setting
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparams
-num_classes = 100
-epochs = 60
+num_classes = 500
+epochs = 100
 batch_size = 32
 learning_rate = 1e-4
-log_interval = 20
+log_interval = 100
 img_d, img_h, img_w = 16, 128, 128
-drop_p = 0.0
-hidden1, hidden2 = 512, 256
 
 # Train with Conv+LSTM
 if __name__ == '__main__':
@@ -51,8 +49,8 @@ if __name__ == '__main__':
     dataset = CSL_Isolated(data_path=data_path, label_path=label_path, frames=img_d, num_classes=num_classes, transform=transform)
     trainset, testset = random_split(dataset, [int(0.8*len(dataset)), int(0.2*len(dataset))])
     logger.info("Dataset samples: {}".format(len(dataset)))
-    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
-    testloader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     # Create model
     model = CRNN(img_depth=img_d, img_height=img_h, img_width=img_w, num_classes=num_classes).to(device)
     # Run the model parallelly
@@ -73,7 +71,7 @@ if __name__ == '__main__':
         test(model, criterion, testloader, device, epoch, logger, writer)
 
         # Save model
-        torch.save(model.state_dict(), os.path.join(model_path, "slr_convlstm_epoch{}.pth".format(epoch+1)))
+        torch.save(model.state_dict(), os.path.join(model_path, "slr_convlstm_epoch{:03d}.pth".format(epoch+1)))
         logger.info("Epoch {} Model Saved".format(epoch+1).center(60, '#'))
 
     logger.info("Training Finished".center(60, '#'))
