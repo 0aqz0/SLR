8294f083909306886f9ff74660b8ab0f17641ec0
0aqz0 <18026332256@163.com>
2020-02-10 10:40:25 +0800

    3d resnet18 achieves acc 98.72% on 100 classes at epoch 32

--- CSL_Isolated_Conv3D.py
+++ CSL_Isolated_Conv3D.py
@@ -9,17 +9,17 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 import torchvision.transforms as transforms
-from models.Conv3D import CNN3D
+from models.Conv3D import CNN3D, resnet18
 from dataset import CSL_Isolated
 from train import train
 from test import test
 
 # Path setting
-data_path = "/home/aistudio/data/data20273/CSL_Isolated_125000"
-label_path = "/home/aistudio/data/data20273/CSL_Isolated_125000/dictionary.txt"
-model_path = "."
-log_path = "log/{:%Y-%m-%d_%H-%M-%S}.log".format(datetime.now())
-sum_path = "runs/slr_{:%Y-%m-%d_%H-%M-%S}".format(datetime.now())
+data_path = "/home/haodong/Data/CSL_Isolated_1/color_video_125000"
+label_path = "/home/haodong/Data/CSL_Isolated_1/dictionary.txt"
+model_path = "/home/haodong/Data/cnn3d_models"
+log_path = "log/cnn3d_{:%Y-%m-%d_%H-%M-%S}.log".format(datetime.now())
+sum_path = "runs/slr_cnn3d_{:%Y-%m-%d_%H-%M-%S}".format(datetime.now())
 
 # Log to file & tensorboard writer
 logging.basicConfig(level=logging.INFO, format='%(message)s', handlers=[logging.FileHandler(log_path), logging.StreamHandler()])
@@ -28,34 +28,36 @@ logger.info('Logging to file...')
 writer = SummaryWriter(sum_path)
 
 # Use specific gpus
-# os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 # Device setting
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparams
-num_classes = 500
+num_classes = 100
 epochs = 50
-batch_size = 16
-learning_rate = 1e-4
-log_interval = 100
-img_d, img_h, img_w = 16, 128, 128
+batch_size = 32
+learning_rate = 1e-5
+log_interval = 20
+sample_size = 128
+sample_duration = 16
 drop_p = 0.0
 hidden1, hidden2 = 512, 256
 
 # Train with 3DCNN
 if __name__ == '__main__':
     # Load data
-    transform = transforms.Compose([transforms.Resize([img_h, img_w]),
+    transform = transforms.Compose([transforms.Resize([sample_size, sample_size]),
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.5], std=[0.5])])
-    dataset = CSL_Isolated(data_path=data_path, label_path=label_path, frames=img_d, num_classes=num_classes, transform=transform)
+    dataset = CSL_Isolated(data_path=data_path, label_path=label_path, frames=sample_duration, num_classes=num_classes, transform=transform)
     trainset, testset = random_split(dataset, [int(0.8*len(dataset)), int(0.2*len(dataset))])
     logger.info("Dataset samples: {}".format(len(dataset)))
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
     testloader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
     # Create model
-    model = CNN3D(img_depth=img_d, img_height=img_h, img_width=img_w, drop_p=drop_p,
-                hidden1=hidden1, hidden2=hidden2, num_classes=num_classes).to(device)
+    # model = CNN3D(img_depth=sample_duration, img_height=sample_size, img_width=sample_size, drop_p=drop_p,
+    #             hidden1=hidden1, hidden2=hidden2, num_classes=num_classes).to(device)
+    model = resnet18(sample_size=sample_size, sample_duration=sample_duration, num_classes=num_classes).to(device)
     # Run the model parallelly
     if torch.cuda.device_count() > 1:
         logger.info("Using {} GPUs".format(torch.cuda.device_count()))
