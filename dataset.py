01283f62a6ab2d9b79a45cec9f459ddd8e10b10d
0aqz0 <18026332256@163.com>
2020-04-21 10:28:54 +0800

    test CSL continuous dataset

--- dataset.py
+++ dataset.py
@@ -200,25 +200,123 @@ class CSL_Continuous(Dataset):
         self.corpus_path = corpus_path
         self.train = train
         self.transform = transform
+        self.num_sentences = 100
         self.signers = 50
         self.repetition = 5
+        if self.train:
+            self.videos_per_folder = int(0.8 * self.signers * self.repetition)
+        else:
+            self.videos_per_folder = int(0.2 * self.signers * self.repetition)
+        # dictionary
+        self.dict = {}
+        try:
+            dict_file = open(self.dict_path, 'r')
+            for line in dict_file.readlines():
+                line = line.strip()
+                line = line.split('\t')
+                index = int(line[0])
+                # word with multiple expressions
+                if '（' in line[1] and '）' in line[1]:
+                    for delimeter in ['（', '）', '、']:
+                        line[1] = line[1].replace(delimeter, " ")
+                    words = line[1].split()
+                else:
+                    words = [line[1]]
+                # print(words)
+                for word in words:
+                    self.dict[word] = index
+        except Exception as e:
+            raise
+        # img data
+        self.data_folder = []
+        try:
+            obs_path = [os.path.join(self.data_path, item) for item in os.listdir(self.data_path)]
+            self.data_folder = sorted([item for item in obs_path if os.path.isdir(item)])
+        except Exception as e:
+            raise
+        # corpus
+        self.corpus = {}
+        self.unknown = set()
+        try:
+            corpus_file = open(self.corpus_path, 'r')
+            for line in corpus_file.readlines():
+                line = line.strip().split()
+                sentence = line[1]
+                raw_sentence = (line[1]+'.')[:-1]
+                paired = [False for i in range(len(line[1]))]
+                # print(id(raw_sentence), id(line[1]), id(sentence))
+                # pair long words with higher priority
+                for token in sorted(self.dict, key=len, reverse=True):
+                    index = raw_sentence.find(token)
+                    # print(index, line[1])
+                    if index != -1 and not paired[index]:
+                        line[1] = line[1].replace(token, " "+token+" ")
+                        # mark as paired
+                        for i in range(len(token)):
+                            paired[index+i] = True
+                tokens = []
+                for token in line[1].split():
+                    if token in self.dict:
+                        tokens.append(self.dict[token])
+                    else:
+                        self.unknown.add(token)
+                self.corpus[line[0]] = tokens
+        except Exception as e:
+            raise
+        # print(self.corpus)
+        # print(self.unknown)
+
+    def read_images(self, folder_path):
+        images = []
+        for i in range(len(os.listdir(folder_path))):
+            image = Image.open(os.path.join(folder_path, '{:06d}.jpg').format(i+1))  #.convert('L')
+            if self.transform is not None:
+                image = self.transform(image)
+            images.append(image)
+
+        images = torch.stack(images, dim=0)
+        # switch dimension
+        images = images.permute(1, 0, 2, 3)
+        # print(images.shape)
+        return images
 
     def __len__(self):
-        pass
+        return self.num_sentences * self.videos_per_folder
 
     def __getitem__(self, idx):
-        pass
+        top_folder = self.data_folder[int(idx/self.videos_per_folder)]
+        selected_folders = [os.path.join(top_folder, item) for item in os.listdir(top_folder)]
+        selected_folders = sorted([item for item in selected_folders if os.path.isdir(item)])
+        if self.train:
+            selected_folder = selected_folders[idx%self.videos_per_folder]
+        else:
+            selected_folder = selected_folders[idx%self.videos_per_folder + int(0.8*self.signers*self.repetition)]
+        images = self.read_images(selected_folder)
+        # print(selected_folder, int(idx/self.videos_per_folder))
+        # print(self.corpus['{:06d}'.format(int(idx/self.videos_per_folder))])
+        tokens = torch.LongTensor(self.corpus['{:06d}'.format(int(idx/self.videos_per_folder))])
+
+        return images, tokens
 
 
 # Test
 if __name__ == '__main__':
     transform = transforms.Compose([transforms.Resize([128, 128]), transforms.ToTensor()])
-    dataset = CSL_Isolated(data_path="/home/haodong/Data/CSL_Isolated/color_video_125000",
-        label_path='/home/haodong/Data/CSL_Isolated/dictionary.txt', transform=transform)    # print(len(dataset))
-    # # print(dataset[1000]['images'].shape)
+    # dataset = CSL_Isolated(data_path="/home/haodong/Data/CSL_Isolated/color_video_125000",
+    #     label_path='/home/haodong/Data/CSL_Isolated/dictionary.txt', transform=transform)    # print(len(dataset))
+    # print(dataset[1000]['images'].shape)
     # dataset = CSL_Skeleton(data_path="/home/haodong/Data/CSL_Isolated/xf500_body_depth_txt",
     #     label_path="/home/haodong/Data/CSL_Isolated/dictionary.txt", selected_joints=['SPINEBASE', 'SPINEMID', 'HANDTIPRIGHT'], split_to_channels=True)
     # print(dataset[1000])
-    label = dataset[1000]['label']
-    print(dataset.label_to_word(label))
+    # label = dataset[1000]['label']
+    # print(dataset.label_to_word(label))
     # dataset[1000]
+    dataset = CSL_Continuous(
+        data_path="/mnt/data/liweijie/public_dataset/color_frame",
+        dict_path="/home/haodong/Data/CSL_Continuous/dictionary.txt",
+        corpus_path="/home/haodong/Data/CSL_Continuous/corpus.txt",
+        train=True, transform=transform
+        )
+    print(len(dataset))
+    images, tokens = dataset[1000]
+    print(images.shape, tokens)
