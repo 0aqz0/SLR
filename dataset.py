98c22e131336f9f127c462d6b3ca69aac58bc207
0aqz0 <18026332256@163.com>
2020-04-26 11:24:43 +0800

    add character level dataset

--- dataset.py
+++ dataset.py
@@ -190,7 +190,7 @@ class CSL_Skeleton(Dataset):
 
 
 """
-Implementation of CSL Continuous Dataset
+Implementation of CSL Continuous Dataset(Word Level)
 """
 class CSL_Continuous(Dataset):
     def __init__(self, data_path, dict_path, corpus_path, frames=128, train=True, transform=None):
@@ -315,6 +315,123 @@ class CSL_Continuous(Dataset):
         return images, tokens
 
 
+"""
+Implementation of CSL Continuous Dataset(Character Level)
+"""
+class CSL_Continuous_Char(Dataset):
+    def __init__(self, data_path, corpus_path, frames=128, train=True, transform=None):
+        super(CSL_Continuous_Char, self).__init__()
+        self.data_path = data_path
+        self.corpus_path = corpus_path
+        self.frames = frames
+        self.train = train
+        self.transform = transform
+        self.num_sentences = 100
+        self.signers = 50
+        self.repetition = 5
+        if self.train:
+            self.videos_per_folder = int(0.8 * self.signers * self.repetition)
+        else:
+            self.videos_per_folder = int(0.2 * self.signers * self.repetition)
+        # dictionary
+        self.dict = {'<pad>': 0, '<sos>': 1, '<eos>': 2}
+        self.output_dim = 3
+        try:
+            dict_file = open(self.corpus_path, 'r')
+            for line in dict_file.readlines():
+                line = line.strip().split()
+                sentence = line[1]
+                for char in sentence:
+                    if char not in self.dict:
+                        self.dict[char] = self.output_dim
+                        self.output_dim += 1
+        except Exception as e:
+            raise
+        # img data
+        self.data_folder = []
+        try:
+            obs_path = [os.path.join(self.data_path, item) for item in os.listdir(self.data_path)]
+            self.data_folder = sorted([item for item in obs_path if os.path.isdir(item)])
+        except Exception as e:
+            raise
+        # corpus
+        self.corpus = {}
+        self.unknown = set()
+        try:
+            corpus_file = open(self.corpus_path, 'r')
+            for line in corpus_file.readlines():
+                line = line.strip().split()
+                sentence = line[1]
+                raw_sentence = (line[1]+'.')[:-1]
+                paired = [False for i in range(len(line[1]))]
+                # print(id(raw_sentence), id(line[1]), id(sentence))
+                # pair long words with higher priority
+                for token in sorted(self.dict, key=len, reverse=True):
+                    index = raw_sentence.find(token)
+                    # print(index, line[1])
+                    if index != -1 and not paired[index]:
+                        line[1] = line[1].replace(token, " "+token+" ")
+                        # mark as paired
+                        for i in range(len(token)):
+                            paired[index+i] = True
+                # add sos
+                tokens = [self.dict['<sos>']]
+                for token in line[1].split():
+                    if token in self.dict:
+                        tokens.append(self.dict[token])
+                    else:
+                        self.unknown.add(token)
+                # add eos
+                tokens.append(self.dict['<eos>'])
+                self.corpus[line[0]] = tokens
+        except Exception as e:
+            raise
+        # add padding
+        length = [len(tokens) for key, tokens in self.corpus.items()]
+        self.max_length = max(length)
+        # print(max(length))
+        for key, tokens in self.corpus.items():
+            if len(tokens) < self.max_length:
+                tokens.extend([self.dict['<pad>']]*(self.max_length-len(tokens)))
+        # print(self.corpus)
+        # print(self.unknown)
+
+    def read_images(self, folder_path):
+        assert len(os.listdir(folder_path)) >= self.frames, "Too few images in your data folder: " + str(folder_path)
+        images = []
+        start = 1
+        step = int(len(os.listdir(folder_path))/self.frames)
+        for i in range(self.frames):
+            image = Image.open(os.path.join(folder_path, '{:06d}.jpg').format(start+i*step))  #.convert('L')
+            if self.transform is not None:
+                image = self.transform(image)
+            images.append(image)
+
+        images = torch.stack(images, dim=0)
+        # switch dimension
+        images = images.permute(1, 0, 2, 3)
+        # print(images.shape)
+        return images
+
+    def __len__(self):
+        return self.num_sentences * self.videos_per_folder
+
+    def __getitem__(self, idx):
+        top_folder = self.data_folder[int(idx/self.videos_per_folder)]
+        selected_folders = [os.path.join(top_folder, item) for item in os.listdir(top_folder)]
+        selected_folders = sorted([item for item in selected_folders if os.path.isdir(item)])
+        if self.train:
+            selected_folder = selected_folders[idx%self.videos_per_folder]
+        else:
+            selected_folder = selected_folders[idx%self.videos_per_folder + int(0.8*self.signers*self.repetition)]
+        images = self.read_images(selected_folder)
+        # print(selected_folder, int(idx/self.videos_per_folder))
+        # print(self.corpus['{:06d}'.format(int(idx/self.videos_per_folder))])
+        tokens = torch.LongTensor(self.corpus['{:06d}'.format(int(idx/self.videos_per_folder))])
+
+        return images, tokens
+
+
 # Test
 if __name__ == '__main__':
     transform = transforms.Compose([transforms.Resize([128, 128]), transforms.ToTensor()])
@@ -327,9 +444,14 @@ if __name__ == '__main__':
     # label = dataset[1000]['label']
     # print(dataset.label_to_word(label))
     # dataset[1000]
-    dataset = CSL_Continuous(
+    # dataset = CSL_Continuous(
+    #     data_path="/home/haodong/Data/CSL_Continuous/color",
+    #     dict_path="/home/haodong/Data/CSL_Continuous/dictionary.txt",
+    #     corpus_path="/home/haodong/Data/CSL_Continuous/corpus.txt",
+    #     train=True, transform=transform
+    #     )
+    dataset = CSL_Continuous_Char(
         data_path="/home/haodong/Data/CSL_Continuous/color",
-        dict_path="/home/haodong/Data/CSL_Continuous/dictionary.txt",
         corpus_path="/home/haodong/Data/CSL_Continuous/corpus.txt",
         train=True, transform=transform
         )
